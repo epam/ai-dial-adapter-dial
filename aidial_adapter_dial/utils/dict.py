179f08aa88706b77e0338f9b11a4f128f5afac02
Anton Dubovik <anton_dubovik@epam.com>
2024-07-22 17:40:53 +0100

    fix: replaced redacted value in favour of one used in Pydantic SecretStr

--- aidial_adapter_dial/utils/dict.py
+++ aidial_adapter_dial/utils/dict.py
@@ -4,5 +4,5 @@ from typing import List, Mapping
 def censor_ci_dict(d: Mapping[str, str], keys: List[str]) -> dict:
     key_set = {k.lower() for k in keys}
     return {
-        k: v if k.lower() not in key_set else "<redacted>" for k, v in d.items()
+        k: v if k.lower() not in key_set else "**********" for k, v in d.items()
     }
